8a43e0ddcb03f2fabe4e65ff609034068188f7c0
konarjg <konarjggames@gmail.com>
2023-05-27 10:44:44 +0200

    Added recipes and a new crystal tier of drill

--- java/com/github/konarjg/arcticraft/item/ItemDrill.java
+++ java/com/github/konarjg/arcticraft/item/ItemDrill.java
@@ -1,9 +1,10 @@
 package com.github.konarjg.arcticraft.item;
 
-import com.github.konarjg.arcticraft.Main;
+import java.util.List;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
+import com.github.konarjg.arcticraft.Main;
+import com.github.konarjg.arcticraft.registry.Items;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
@@ -11,14 +12,18 @@ import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.common.TierSortingRegistry;
 
 public class ItemDrill extends DiggerItem
 {
 	public static final TagKey<Block> DRILL = BlockTags.create(new ResourceLocation(Main.MODID, "mineable/drill"));
-	
+	public static final TagKey<Block> NEEDS_CRYSTAL_TOOL = BlockTags.create(new ResourceLocation(Main.MODID, "needs_crystal_tool"));
+	public static final Tier CRYSTAL = TierSortingRegistry.registerTier(new ForgeTier(6, 10000, 20, 10, 100, NEEDS_CRYSTAL_TOOL, () -> Ingredient.of(Items.CRYSTAL.get())), new ResourceLocation(Main.MODID, "crystal_tier"), List.of(Tiers.NETHERITE), List.of());
+		
 	public ItemDrill(Tier tier) 
 	{
 		super(2, 0.75f, tier, DRILL, new Properties());
@@ -37,6 +42,8 @@ public class ItemDrill extends DiggerItem
 			return 12f;
 		else if (getTier() == Tiers.NETHERITE)
 			return 15f;
+		else if (getTier() == CRYSTAL)
+			return 20f;
 		
 		return 2f;
 	}
--- java/com/github/konarjg/arcticraft/registry/Items.java
+++ java/com/github/konarjg/arcticraft/registry/Items.java
@@ -3,9 +3,12 @@ package com.github.konarjg.arcticraft.registry;
 import com.github.konarjg.arcticraft.Main;
 import com.github.konarjg.arcticraft.item.ItemDrill;
 
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ShovelItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -25,10 +28,12 @@ public final class Items
 	public static final RegistryObject<Item> HARD_ICE = ITEMS.register("hard_ice", () -> new BlockItem(Blocks.HARD_ICE.get(), new Item.Properties()));
 	public static final RegistryObject<Item> CRYSTAL_ORE = ITEMS.register("crystal_ore", () -> new BlockItem(Blocks.CRYSTAL_ORE.get(), new Item.Properties()));
 	public static final RegistryObject<Item> CRYSTAL_BLOCK = ITEMS.register("crystal_block", () -> new BlockItem(Blocks.CRYSTAL_BLOCK.get(), new Item.Properties()));
+	
 	//Tools
 	public static final RegistryObject<Item> STONE_DRILL = ITEMS.register("stone_drill", () -> new ItemDrill(Tiers.STONE));
 	public static final RegistryObject<Item> IRON_DRILL = ITEMS.register("iron_drill", () -> new ItemDrill(Tiers.IRON));
 	public static final RegistryObject<Item> GOLDEN_DRILL = ITEMS.register("golden_drill", () -> new ItemDrill(Tiers.GOLD));
 	public static final RegistryObject<Item> DIAMOND_DRILL = ITEMS.register("diamond_drill", () -> new ItemDrill(Tiers.DIAMOND));
 	public static final RegistryObject<Item> NETHERITE_DRILL = ITEMS.register("netherite_drill", () -> new ItemDrill(Tiers.NETHERITE));
+	public static final RegistryObject<Item> CRYSTAL_DRILL = ITEMS.register("crystal_drill", () -> new ItemDrill(ItemDrill.CRYSTAL));
 }
